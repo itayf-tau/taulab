3018b9f7042d351dad0d99fd985d6bcfd39dd773
Itay Feldman <itay1405@gmail.com>
2025-11-10 20:12:07 +0200

    change datatypes

--- taulab/datatypes.py
+++ taulab/datatypes.py
@@ -1,18 +1,22 @@
 import numpy.typing as npt
 import pandas as pd
 from dataclasses import dataclass
+from collections.abc import Callable
 
 
 @dataclass
 class FitResult:
     """Standard dataclass to hold results from a curve fitting operation."""
-
     params: npt.ArrayLike  # params in their order (a0, a1, a2, ...)
     error: npt.ArrayLike  # errors corresponding to each param
     covariance: npt.ArrayLike
+    function: Callable
+
+    def extrapolate(self, x):
+        return self.function(self.params, x)
 
 
 @dataclass
 class ParseResult:
     data: pd.DataFrame
-    metadata: dict | None = None
+    metadata: dict
