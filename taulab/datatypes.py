758de2ebf74895a0d468b421e8e4ae889c3a7580
Itay Feldman <itay1405@gmail.com>
2025-12-01 15:54:05 +0200

    Add measurement datatype

--- taulab/datatypes.py
+++ taulab/datatypes.py
@@ -1,7 +1,16 @@
-import numpy.typing as npt
+import numpy as np
 import pandas as pd
+import numpy.typing as npt
 from dataclasses import dataclass
 from collections.abc import Callable
+
+
+@dataclass
+class Measurement:
+    x: npt.NDArray[np.float64]
+    y: npt.NDArray[np.float64]
+    x_err: npt.NDArray[np.float64] | None = None
+    y_err: npt.NDArray[np.float64] | None = None
 
 
 @dataclass
